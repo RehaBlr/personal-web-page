390c068c17691307561257495dab72cd8ab2f2bd
Reha <134259610+RehaBlr@users.noreply.github.com>
2023-08-13 21:53:11 +0300

    skills component

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import "./App.css";
 
 import Header from "./components/Header";
 import About from "./components/About";
+import Skills from "./components/Skills";
 
 function App() {
-  const [toggle, setToggle] = useState(true);
+  const [toggle, setToggle] = useState(false);
   // console.log(toggle);
   return (
     <div className={toggle ? "App dark" : "App"}>
@@ -13,6 +14,8 @@ function App() {
         <Header toggle={toggle} setToggle={setToggle} />
 
         <About />
+
+        <Skills />
       </div>
     </div>
   );
---
+++ src/components/Skills.js
@@ -0,0 +1,35 @@
+export default function Skills() {
+  return (
+    <>
+      <div className="containerSkills">
+        <div>
+          <h2 id="skills">Skills</h2>
+        </div>
+
+        <div className="skillText">
+          <div>
+            <h3>Java Script</h3>
+            <p>
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+              officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
+          <div>
+            <h3>React.Js</h3>
+            <p>
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+              officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
+          <div>
+            <h3>Node.Js</h3>
+            <p>
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+              officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
